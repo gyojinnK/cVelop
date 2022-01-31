e463b96f8b2ae7e7df766a5393cf4e9b96701927
gyojinnK <ksmug1998@gmail.com>
2022-02-01 00:25:46 +0900

    Add FrontToBack commit

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,36 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect} from "react";
+import FrontToBack from './FrontToBack.js';
 
 function App() {
-  const [message, setMessage] = useState([]);
-
-  useEffect(()=>{
-    fetch("/hello").then((res)=>{
-      return res.json();
-    }).then((data) => {
-      setMessage(data);
-    })
-  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ul>
-          {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
-        </ul>
+        <FrontToBack />
       </header>
     </div>
   );
